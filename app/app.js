978764abaa3bbf53a09dbe469d1cd3aee9029b7a
Hemanth <hemanthk97@yahoo.com>
2015-11-28 17:38:40 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -9,13 +9,13 @@
     var show = "";
     app.controller("StoreController", ['$scope', '$http', 'cfpLoadingBar', function ($scope, $http, cfpLoadingBar) {
         cfpLoadingBar.start();
-        $http.get("http://dev.shopalyst.com/shopalyst-service/hackerearth002/1.0/products?limit=100").success(function (data) {
+        $http.get("https://dev.shopalyst.com/shopalyst-service/hackerearth002/1.0/products?limit=100").success(function (data) {
             $scope.products = data;
             });
 
         $scope.loadFeed = function (e) {
             cfpLoadingBar.start();
-            $http.get("http://dev.shopalyst.com/shopalyst-service/hackerearth002/1.0/products/"+e).success(function (data) {
+            $http.get("https://dev.shopalyst.com/shopalyst-service/hackerearth002/1.0/products/"+e).success(function (data) {
                 $scope.sepProduct = data;
                 $scope.show = "success";
                 });
